5a4c97b6c53081f3515fe23afdc902722694cca2
sravanneeli <sravan.neeli267@gmail.com>
2020-03-10 22:50:56 +0530

    merge give intervals

---
+++ merge_given_intervals.py
@@ -0,0 +1,18 @@
+def merge(intervals):
+    intervals.sort(key=lambda x: x[0])
+    output = [intervals[0]]
+    for i in range(1, len(intervals)):
+        if intervals[i][0] <= output[-1][1]:
+            output[-1][1] = max(intervals[i][1], output[-1][1])
+        else:
+            output.append(intervals[i])
+    return output
+
+
+def main():
+    new_list = merge([[1, 2], [3, 5], [3, 7], [1, 100]])
+    print("New intervals after merging", new_list)
+
+
+if __name__ == "__main__":
+    main()
